900ff131e0f805b8915acbc55a4c022f69da4a3e
andersonroberto91 <andersonroberto.x91>
2023-10-05 21:18:38 -0300

    feat: create files eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from "./app";
-import { envSchema } from "./env";
+import { app } from './app';
+import { env } from './env';
 
-app.listen({
-  host: '0.0.0.0',
-  port: envSchema.PORT
-}).then(() => {console.log('Server starded.')})
-
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server starded.');
+  });
